0e58a28346850e426f6eef72017a8e05bf11290c
GerlandeSousaVaranda <111927646+GerlandeSousaVaranda@users.noreply.github.com>
2022-12-24 22:10:59 -0300

    ativi 31
    ativ 31

--- atividade31/scriptjs/jvscript.js
+++ atividade31/scriptjs/jvscript.js
@@ -25,6 +25,24 @@ function raizquadrada(num){
 }
 
 function escreve( ){
-	var texto = prompt('digite um texto qualquer : ')
+	var texto = prompt('digite um texto qualquer : ');
 	document.getElementById("minhaDiv").innerHTML = texto;
 }
+
+function seq (){
+	const lista = [];
+	let n = prompt('digite um numero : ');
+	for(let x = 1; x <= n; x++){
+		lista.push(x);
+		document.getElementById("minhadiv").innerHTML = lista;
+	}
+}
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
